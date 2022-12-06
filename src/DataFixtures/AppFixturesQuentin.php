c0efe27d1d91d29fc7474fbe394aec33478f544d
qguizouarn2021 <quentin.guizouarn2021@campus-eni.fr>
2022-12-06 14:31:34 +0100

    Fixtures (problème avec text infoSortie --mis en commentaire)

--- src/DataFixtures/AppFixturesQuentin.php
+++ src/DataFixtures/AppFixturesQuentin.php
@@ -8,7 +8,6 @@ use App\Entity\Lieu;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Entity\Ville;
-use App\Repository\VilleRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -27,12 +26,24 @@ class AppFixturesQuentin extends Fixture
     public function load(ObjectManager $manager): void
     {
         $this->manager = $manager;
-        $this->ajouterCampus(5);
-        $this->ajouterVille(20);
-        $this->ajouterLieu(50);
-        $this->ajouterEtat();
-        //$this->ajouterParticipant(60);
-        //$this->ajouterSortie(50);
+        $this->ajouterEtats();
+        $this->ajouterCampus(10);
+        $this->ajouterVilles(50);
+        $this->ajouterLieus(25);
+        $this->ajouterParticipants(120);
+        //$this->ajouterSorties(20);
+    }
+
+    public function ajouterEtats(): void
+    {
+        $libelles = ['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée', 'Historisée'];
+        for ($i = 0; $i < count($libelles); $i++)
+        {
+            $etat = new Etat();
+            $etat->setLibelle($libelles[$i]);
+            $this->manager->persist($etat);
+        }
+        $this->manager->flush();
     }
 
     public function ajouterCampus(int $nb): void
@@ -46,7 +57,7 @@ class AppFixturesQuentin extends Fixture
         $this->manager->flush();
     }
 
-    public function ajouterVille(int $nb): void
+    public function ajouterVilles(int $nb): void
     {
         for ($i = 0; $i < $nb; $i++)
         {
@@ -58,13 +69,14 @@ class AppFixturesQuentin extends Fixture
         $this->manager->flush();
     }
 
-    public function ajouterLieu(int $nb): void
+    public function ajouterLieus(int $nb): void
     {
-        $villes = $this->manager->getRepository(VilleRepository::class)->findAll();
+        $villes = $this->manager->getRepository(Ville::class)->findAll();
+
         for ($i = 0; $i < $nb; $i++)
         {
             $lieu = new Lieu();
-            $lieu->setNom($this->faker->text(120));
+            $lieu->setNom($this->faker->company);
             $lieu->setRue($this->faker->streetAddress);
             $lieu->setLatitude($this->faker->latitude);
             $lieu->setLongitude($this->faker->longitude);
@@ -74,32 +86,59 @@ class AppFixturesQuentin extends Fixture
         $this->manager->flush();
     }
 
-    public function ajouterEtat(): void
+    public function ajouterParticipants(int $nb): void
     {
-        $libelles = ['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée', 'Historisée'];
-        for ($i = 0; $i < count($libelles); $i++)
-        {
-            $etat = new Etat();
-            $etat->setLibelle($libelles[$i]);
-            $this->manager->persist($etat);
-        }
-        $this->manager->flush();
-    }
+        $campus = $this->manager->getRepository(Campus::class)->findAll();
 
-    public function ajouterParticipant(int $nb): void
-    {
         for ($i = 0; $i < $nb; $i++)
         {
             $participant = new Participant();
+            $participant->setNom($this->faker->lastName);
+            $participant->setPrenom($this->faker->firstName);
+            $participant->setTelephone($this->faker->phoneNumber);
+            $participant->setEmail($this->faker->email);
+            var_dump($participant->getEmail());
+            $participant->setPseudo($this->faker->userName);
+            var_dump($participant->getPseudo());
+            $goodPassword = $this->faker->password(8, 20);
+            var_dump($goodPassword);
+            //$participant->setPassword($this->hasher->hashPassword($participant, $goodPassword));
+            $participant->setPassword($goodPassword);
+            $participant->setActif($this->faker->boolean(99));
+            $participant->setCampus($this->faker->randomElement($campus));
+            $this->manager->persist($participant);
         }
         $this->manager->flush();
     }
 
-    public function ajouterSortie(int $nb): void
+    public function ajouterSorties(int $nb): void
     {
+        $etats = $this->manager->getRepository(Etat::class)->findAll();
+        $lieus = $this->manager->getRepository(Lieu::class)->findAll();
+        $campus = $this->manager->getRepository(Campus::class)->findAll();
+        $participants = $this->manager->getRepository(Participant::class)->findAll();
+
         for ($i = 0; $i < $nb; $i++)
         {
             $sortie = new Sortie();
+            $sortie->setNom($this->faker->name);
+            $sortie->setDateHeureDebut($this->faker->dateTimeBetween("+3 months", "+8 months"));
+            $sortie->setDuree($this->faker->numberBetween(60, 240));
+            $sortie->setDateLimiteInscription($this->faker->dateTimeBetween("now", "+2 months"));
+            $nbP = $this->faker->numberBetween(0, 16);
+            $sortie->setNbInscriptionsMax($nbP);
+            $sortie->setInfoSortie($this->faker->text);
+            $sortie->setEtat($this->faker->randomElement($etats));
+            $sortie->setLieu($this->faker->randomElement($lieus));
+            $sortie->setCampus($this->faker->randomElement($campus));
+            $organisateur = $this->faker->randomElement($participants);
+            $sortie->setOrganisateur($organisateur);
+            $sortie->addParticipant($organisateur);
+            for ($i = 1; $i < $nbP; $i++)
+            {
+                $sortie->addParticipant($this->faker->randomElement($participants));
+            }
+            $this->manager->persist($sortie);
         }
         $this->manager->flush();
     }
