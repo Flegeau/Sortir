7cc782e99b6ed39bdcf9e571905fdac658bb30c3
qguizouarn2021 <quentin.guizouarn2021@campus-eni.fr>
2022-12-13 13:58:43 +0100

    Contrôle connexion dans SortieController.php amélioré

--- src/Service/ControleSortie.php
+++ src/Service/ControleSortie.php
@@ -14,8 +14,28 @@ class ControleSortie {
     public const MESSAGE_MODIFICATION = 'La sortie a été modifiée';
     public const MESSAGE_SUPPRESSION = 'La sortie a été supprimée';
 
-    private const ETATS_NON_AFFICHABLES = array('Créée', 'Annulée', 'Historisée');
-    private const ETAT_MODIFIABLE = 'Créée';
+    private const ETAT_CREE = 'Créée';
+    private const ETAT_OUVERT = 'Ouverte';
+    private const ETAT_CLOTURE = 'Clôturée';
+    private const ETAT_EN_COURS = 'Activité en cours';
+    private const ETAT_PASSE = 'Passée';
+    private const ETAT_ANNULE = 'Annulée';
+    private const ETAT_HISTORISE = 'Historisée';
+
+    private const ETATS_NON_AFFICHABLES = array(
+        self::ETAT_CREE,
+        self::ETAT_ANNULE,
+        self::ETAT_HISTORISE
+    );
+    private const ETATS_ANNULABLES = array(
+        self::ETAT_OUVERT,
+        self::ETAT_CLOTURE
+    );
+    private const ETATS_HISTORISABLES = array(
+        self::ETAT_PASSE,
+        self::ETAT_ANNULE
+    );
+
     private \DateTime $date;
 
     public function __construct()
@@ -34,13 +54,64 @@ class ControleSortie {
 
     public function estModifiable(Sortie $sortie): bool
     {
-        if ($sortie->getEtat()->getLibelle() !== self::ETAT_MODIFIABLE ||
+        if ($sortie->getEtat()->getLibelle() !== self::ETAT_CREE ||
             $this->date > $sortie->getDateLimiteInscription() ||
             $this->date > $sortie->getDateHeureDebut())
         {
             return false;
         }
         return true;
+    }
+
+    public function estInscrivable(Sortie $sortie): bool
+    {
+        if ($sortie->getEtat()->getLibelle() !== self::ETAT_OUVERT &&
+            (count($sortie->getParticipants()) === $sortie->getNbInscriptionsMax() ||
+            $this->date > $sortie->getDateLimiteInscription()))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public function estDesistable(Sortie $sortie): bool
+    {
+        if (!$this->estInscrivable($sortie) ||
+            ($sortie->getEtat()->getLibelle() !== self::ETAT_CLOTURE &&
+            $this->date > $sortie->getDateLimiteInscription()))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public function estAnnulable(Sortie $sortie): bool
+    {
+        if (!in_array($sortie->getEtat()->getLibelle(), self::ETATS_ANNULABLES))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public function estHistorisable(Sortie $sortie): bool
+    {
+        if (!in_array($sortie->getEtat()->getLibelle(), self::ETATS_HISTORISABLES) &&
+            $this->obtenirDateLimiteHistorisation() > $this->obtenirDateFinSortie($sortie))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private function obtenirDateFinSortie(Sortie $sortie): \DateTime
+    {
+        return $sortie->getDateHeureDebut()->modify('+'.$sortie->getDuree().' minutes');
+    }
+
+    private function obtenirDateLimiteHistorisation(): \DateTime
+    {
+        return $this->date->modify('-1 month');
     }
 
 }
