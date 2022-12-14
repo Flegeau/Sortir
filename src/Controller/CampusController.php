5f89e59bfb32bac89657dfcc80abaf3cf39b6d38
qguizouarn2021 <quentin.guizouarn2021@campus-eni.fr>
2022-12-14 14:15:46 +0100

    Ajout données campus (crud)

---
+++ src/Controller/CampusController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Form\CampusType;
+use App\Repository\CampusRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/campus')]
+class CampusController extends AbstractController
+{
+    #[Route('/', name: 'app_campus_index', methods: ['GET', 'POST'])]
+    public function index(Request $request, CampusRepository $campusRepository): Response
+    {
+        $campus = new Campus();
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $campusRepository->save($campus, true);
+            return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('campus/index.html.twig', [
+            'campuses' => $campusRepository->findAll(),
+            'campus' => $campus,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/modifier', name: 'app_campus_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Campus $campus, CampusRepository $campusRepository): Response
+    {
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $campusRepository->save($campus, true);
+
+            return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('campus/edit.html.twig', [
+            'campus' => $campus,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_campus_delete', methods: ['POST'])]
+    public function delete(Request $request, Campus $campus, CampusRepository $campusRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$campus->getId(), $request->request->get('_token'))) {
+            $campusRepository->remove($campus, true);
+        }
+
+        return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
