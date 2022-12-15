288c6b73fb670c1b0aa239317fc2080ed8602edf
qguizouarn2021 <quentin.guizouarn2021@campus-eni.fr>
2022-12-15 14:28:26 +0100

    Gestion message et suppression campus (qui n'ont pas de sorties)

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -15,9 +15,18 @@ class CampusController extends AbstractController
 {
     private const MESSAGE_LOGIN = 'Vous devez d\'abord vous connecter';
     private const MESSAGE_NON_ADMIN = 'Vous n\'avez pas les droits pour accéder à cette page';
+    private const MESSAGE_AJOUT = 'Le campus a été créé';
+    private const MESSAGE_SUPPRESSION = 'Le campus a été supprimé';
+
+    private CampusRepository $campusRepository;
+
+    public function __construct(CampusRepository $campusRepository)
+    {
+        $this->campusRepository = $campusRepository;
+    }
 
     #[Route('/', name: 'app_campus_index', methods: ['GET', 'POST'])]
-    public function index(Request $request, CampusRepository $campusRepository): Response
+    public function index(Request $request): Response
     {
         if (!$this->getUser()) {
             $this->addFlash('danger', self::MESSAGE_LOGIN);
@@ -33,19 +42,20 @@ class CampusController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $campusRepository->save($campus, true);
+            $this->campusRepository->save($campus, true);
+            $this->addFlash('success', self::MESSAGE_AJOUT);
             return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('campus/index.html.twig', [
-            'campuses' => $campusRepository->findBy([], array('nom' => 'asc')),
+            'campuses' => $this->campusRepository->findBy([], array('nom' => 'asc')),
             'campus' => $campus,
             'form' => $form,
         ]);
     }
 
     #[Route('/{id}', name: 'app_campus_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Campus $campus, CampusRepository $campusRepository): Response
+    public function edit(Request $request, Campus $campus): Response
     {
         if (!$this->getUser()) {
             $this->addFlash('danger', self::MESSAGE_LOGIN);
@@ -60,8 +70,7 @@ class CampusController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $campusRepository->save($campus, true);
-
+            $this->campusRepository->save($campus, true);
             return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -69,5 +78,25 @@ class CampusController extends AbstractController
             'campus' => $campus,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}/supprimer', name: 'app_campus_delete', requirements: ['id'=> '\d+'], methods: ['POST'])]
+    public function delete(Request $request, Campus $campus): Response
+    {
+        if (!$this->getUser()) {
+            $this->addFlash('danger', self::MESSAGE_LOGIN);
+            return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
+        } else {
+            if (!in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
+                $this->addFlash('danger', self::MESSAGE_NON_ADMIN);
+                return $this->redirectToRoute('app_sortie_list', [], Response::HTTP_SEE_OTHER);
+            }
+        }
+        if ($this->isCsrfTokenValid('delete'.$campus->getId(), $request->request->get('_token_delete'))) {
+            $this->campusRepository->remove($campus, true);
+            $this->addFlash('info', self::MESSAGE_SUPPRESSION);
+        }
+
+        return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
     }
 }
